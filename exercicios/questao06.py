a8afb150c84736d4e89040b43fa9a0037eed6f52
nathil <nathiellycastro282@gmail.com>
2025-03-02 00:54:36 -0300

    add comentarios

--- exercicios/questao06.py
+++ exercicios/questao06.py
@@ -9,6 +9,9 @@ dados = " "
 
 with open(caminho, "r", encoding="utf-8") as arquivo: 
     dados = arquivo.read() 
+    #metodo read lê todo o arquivo e retorna uma string contendo o arquivo inteiro
+    #metodo readline lê uma linha do arquivo e retorna a linha como string
+    #metodo readlines lê todo o arquivo e retonar uma lista onde cada elemento é uma string de cada linha do arquivo
 
 with open("novo_" + caminho, "w", encoding="utf-8") as arquivo: 
     arquivo.write(re.sub("[aeiouAEIOU]", "*", dados)) 
